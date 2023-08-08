b75b51bed065c0a8cbcb6f6ef97c695f760c7fc9
yogiifr <yogiifr@gmail.com>
2023-08-08 20:08:25 +0700

    Fresh Start Add a couple intro section Next step would be make it responsive, Add interactive custom cursor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,82 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from "react";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const texts = [
+    "Welcome to Yogi's Portfolio Website",
+    "But sorry, this website is actually under construction 🙏",
+    "Yogi is still figuring out how to build an epic website :D",
+    "About to use this technology on this website",
+    "See you again when all things are packed up",
+    "Yet still, you can keep in touch with Yogi",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animationDirection, setAnimationDirection] = useState(null);
+
+  const handleClick = () => {
+    if (currentIndex < texts.length - 1) {
+      setAnimationDirection("up"); // Trigger the slide-up animation
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+        setAnimationDirection("down"); // Trigger the slide-down animation
+      }, 500); // Adjust this delay to match your animation duration
+    }
+  };
+
+  const renderImageSection = () => {
+    if (currentIndex === 3) {
+      return (
+        <div className="mt-5 text-center justify-between">
+          <div className="flex gap-4 items-center justify-center">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png" alt="Javascript Logo" className=" h-16 mr-4" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png" alt="Typescript Logo" className="h-16 mr-4" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="React Logo" className="h-16" />
+            <img src="https://devtop.io/wp-content/uploads/2022/11/Next-JS.png" alt="Next.JS Logo" className="h-16" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png" alt="Tailwind CSS Logo" className="h-11" />
+          </div>
+          <div className="text-[#525252] text-xs block justify-center items-center mt-5">JAVASCRIPT | TYPESCRIPT | REACT | NEXT.JS | TAILWINDCSS</div>
         </div>
+      );
+    }
+    return null;
+  };
+
+  const renderContactSection = () => {
+    if (currentIndex === 5) {
+      return (
+        <div className="mt-[-320px] text-center">
+          <div className="flex">
+            <a href="https://www.linkedin.com/in/yogiifr/" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-128.png" alt="LinkedIn Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
+            </a>
+            <a href="https://www.instagram.com/yogiifr" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-128.png" alt="Instagram Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
+            </a>
+            <a href="https://www.behance.net/yogifr" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn3.iconfinder.com/data/icons/social-round-corner/512/behance__social__media-64.png" alt="Behance Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
+            </a>
+            <a href="mailto:yogiffrr@gmail.com">
+              <img src="https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/Email-64.png" alt="Email Logo" className="h-10 transition ease-in-out hover:scale-110" />
+            </a>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <main className={`flex min-h-screen flex-col items-center justify-between p-10 bg-white ${inter.className}`}>
+      <div className="text-[#525252] text-xs">FRONT END DEVELOPER</div>
+      <div className={`text-black text-2xl cursor-pointer transition-transform transform hover:scale-105 animate-${animationDirection}`} onClick={handleClick} onTransitionEnd={() => setAnimationDirection(null)}>
+        {texts[currentIndex]}
+        {renderImageSection()}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {renderContactSection()}
+      <div className="text-[#525252] text-xs">© Yogi Fitriadi Rakhim, 2023</div>
     </main>
-  )
+  );
 }
