85323b6b8dfb473c6bf11af21b01230ce52732ee
yogiifr <yogiifr@gmail.com>
2023-08-09 16:06:33 +0700

    Add some new touch up Added some web description including favicon Added GITHUB contact And made it mobile suppoer

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Yogi Fitriadi Rakhim</title>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,13 +31,13 @@ export default function Home() {
       return (
         <div className="mt-5 text-center justify-between">
           <div className="flex gap-4 items-center justify-center">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png" alt="Javascript Logo" className=" h-16 mr-4" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png" alt="Typescript Logo" className="h-16 mr-4" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="React Logo" className="h-16" />
-            <img src="https://devtop.io/wp-content/uploads/2022/11/Next-JS.png" alt="Next.JS Logo" className="h-16" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png" alt="Tailwind CSS Logo" className="h-11" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png" alt="Javascript Logo" className="h-8 sm:h-16 sm:mr-4" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png" alt="Typescript Logo" className="h-10 sm:h-16 sm:mr-4" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="React Logo" className="h-8 sm:h-16 sm:mr-4" />
+            <img src="https://devtop.io/wp-content/uploads/2022/11/Next-JS.png" alt="Next.JS Logo" className="h-8 sm:h-16 sm:mr-4" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png" alt="Tailwind CSS Logo" className="h-8 sm:h-16 sm:mr-4" />
           </div>
-          <div className="text-[#525252] text-xs block justify-center items-center mt-5">JAVASCRIPT | TYPESCRIPT | REACT | NEXT.JS | TAILWINDCSS</div>
+          <div className="text-[#525252] text-[10px] sm:text-xs block justify-center items-center mt-5 ">JAVASCRIPT | TYPESCRIPT | REACT | NEXT.JS | TAILWINDCSS</div>
         </div>
       );
     }
@@ -47,13 +47,16 @@ export default function Home() {
   const renderContactSection = () => {
     if (currentIndex === 5) {
       return (
-        <div className="mt-[-320px] text-center">
+        <div className="mt-[-260px] sm:mt-[-320px] text-center">
           <div className="flex">
             <a href="https://www.linkedin.com/in/yogiifr/" target="_blank" rel="noopener noreferrer">
               <img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-128.png" alt="LinkedIn Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
             </a>
             <a href="https://www.instagram.com/yogiifr" target="_blank" rel="noopener noreferrer">
               <img src="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Instagram_svg-128.png" alt="Instagram Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
+            </a>
+            <a href="https://github.com/yogiifr" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn0.iconfinder.com/data/icons/font-awesome-brands-vol-1/512/github-square-64.png" alt="Github Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
             </a>
             <a href="https://www.behance.net/yogifr" target="_blank" rel="noopener noreferrer">
               <img src="https://cdn3.iconfinder.com/data/icons/social-round-corner/512/behance__social__media-64.png" alt="Behance Logo" className="h-10 mr-4 transition ease-in-out hover:scale-110" />
@@ -76,7 +79,7 @@ export default function Home() {
         {renderImageSection()}
       </div>
       {renderContactSection()}
-      <div className="text-[#525252] text-xs">© Yogi Fitriadi Rakhim, 2023</div>
+      <div className="text-[#525252] text-xs">Yogi Fitriadi Rakhim, 2023 ©</div>
     </main>
   );
 }
